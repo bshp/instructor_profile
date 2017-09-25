8aec1cfdaec03f2044545ed876b81d84b97847b6
Jason A Everling <jason.everling@gmail.com>
2017-09-25 08:18:21 -0500

    add view bio link to user profile

--- lang/en/block_instructor_profile.php
+++ lang/en/block_instructor_profile.php
@@ -2,4 +2,5 @@
 
 $string['pluginname'] = 'Instructor Profile';
 $string['edit'] = 'Edit';
+$string['viewbio'] = 'View Bio';
 $string['instructor_profile:addinstance'] = 'Add Instructor Profile Block';
